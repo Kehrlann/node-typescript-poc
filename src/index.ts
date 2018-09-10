2f97c47cd2635757354e59244cb2eab40af3a7ba
Daniel Garnier-Moiroux <git@garnier.wf>
2018-09-10 20:17:54 +0200

    Add express js for long-running apps, and a watch mode
    - Using nodemon to watch and re-run the compilation + server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import { greet } from "./util";
+import * as express from "express";
 
-greet("world");
+greet("new app");
+
+const app = express();
+app.get("/", (req, res, next) => {
+  greet(req.query.name || "world");
+  res.send("Hello !");
+});
+
+app.listen(3000);
--- src/util.ts
+++ src/util.ts
@@ -3,5 +3,5 @@ import { getLogger } from "./nested/logger";
 const logger = getLogger("greet");
 
 export function greet(name: string) {
-  logger(`Bonjour, ${name} !`);
+  logger(`Hola, ${name} !`);
 }
